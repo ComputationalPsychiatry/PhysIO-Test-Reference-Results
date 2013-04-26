8290f8ca7a547cad84567451a4f2fbf86bfb3442
Lars Kasper <kasper@biomed.ee.ethz.ch>
2013-04-26 10:55:32 +0000

    updated Manual, bug concerning Nprep = [], improved main_ECG3T.m
    git-svn-id: svn+ssh://colombo07.ethz.ch/home/kasperla/svn_repos/smoothing/trunk/tSNR_fMRI_SPM/CheckPhysRETROICOR/PhysIOToolbox@185 d7055aa0-e31f-11dd-b94e-b50442429228

--- examples/Philips/ECG3T/main_ECG3T.m
+++ examples/Philips/ECG3T/main_ECG3T.m
@@ -1,4 +1,5 @@
 %% 0. Put code directory into path; for some options, SPM should also be in the path
+
 pathRETROICORcode = fullfile(fileparts(mfilename('fullpath')), ...
     '../../../code');
 
@@ -11,6 +12,7 @@ sqpar       = physio.sqpar;
 model       = physio.model;
 verbose     = physio.verbose;
 
+
 %% 1. Define Input Files
 
 log_files.vendor            = 'Philips';
@@ -20,39 +22,43 @@ log_files.respiration       = 'SCANPHYSLOG.log';
 
 %% 2. Define Nominal Sequence Parameter (Scan Timing)
 
-% 2.1. Counting scans and dummy volumes from end of run, i.e. logfile
 sqpar.Nslices           = 37;
 sqpar.NslicesPerBeat    = 37;
 sqpar.TR                = 2.50;
 sqpar.Ndummies          = 3;
 sqpar.Nscans            = 495;
 sqpar.onset_slice       = 19;
-
-% 2.2. Counting scans and dummy volumes from beginning of run, i.e. logfile,
-%      includes counting of preparation gradients        
-% (Uncomment the following line to execute) 
-% sqpar.Nprep = 3;
+sqpar.Nprep             = []; % set to >=0 to count scans and dummy 
+                              % volumes from beginning of run, i.e. logfile,
+                              % includes counting of preparation gradients
+sqpar.TimeSliceToSlice  = sqpar.TR / sqpar.Nslices;
 
 
 %% 3. Define Gradient Thresholds to Infer Gradient Timing (Philips only)
 % 3.1. Determine volume start solely by marking every Nslices-th scan slice
 % event as volume event
-thresh.scan_timing = struct('zero', 1700, 'slice', 1800, 'vol', [], ...
- 'grad_direction', 'y');
 
-% 3.2. Alternative: Determine volume start by especially high gradient height thresh.vol
-% (Uncomment the following line to execute) 
-% thresh.scan_timing.vol = 3000;
+use_gradient_log_for_timing = true; % true or false
 
-% 3.3. Alternative: Determine scan events via spacing of volume/slice gradients
-% (Uncomment the following line to execute) 
-% thresh.scan_timing.vol_spacing = 80e-3; % in seconds
-
+if use_gradient_log_for_timing
+    thresh.scan_timing.grad_direction = 'y';
+    thresh.scan_timing.zero         = 1700;
+    thresh.scan_timing.slice        = 1800;
+    thresh.scan_timing.vol          = [];   % leave [], if unused; set value >=.slice,
+                                            % if volume start gradients are higher than slice gradients
+    thresh.scan_timing.vol_spacing  = [];   % leave [], if unused; set to e.g. 50e-3 (seconds),
+                                            % if there is a time gap between last slice of a volume
+                                            % and first slice of the next
+else
+    thresh.scan_timing = [];
+end
+    
 
 %% 4. Define which Cardiac Data Shall be Used
 
-%% 4.1. Using heart beat events logged prospectively during scanning instead
 thresh.cardiac.modality = 'ECG';
+thresh.cardiac.initial_cpulse_select.method = 'load_from_logfile';
+thresh.cardiac.posthoc_cpulse_select.method = 'off';
 
 
 %% 5. Order of RETROICOR-expansions for cardiac, respiratory and
@@ -60,19 +66,15 @@ thresh.cardiac.modality = 'ECG';
 
 model.type = 'RETROICOR';
 model.order = struct('c',3,'r',4,'cr',1, 'orthogonalise', 'none');
-model.input_other_multiple_regressors = 'rp_fMRI.txt'; % either txt-file or mat-file with variable R
+model.input_other_multiple_regressors = 'rp_fMRI.txt'; % either .txt-file or .mat-file (saves variable R)
 model.output_multiple_regressors = 'multiple_regressors.txt';
 
 
 %% 6. Output Figures to be generated
 
-verbose.level = 3;
-% 0 = none; 
-% 1 = main plots (default); 
-% 2 = debugging plots: for missed slice/volume events, missed heartbeats, 1D time series of created regressors
-% 3 = all plots, incl. cardiac/respiratory phase estimation,
-%     slice-to-volume assignment
+verbose.level           = 2; % 0 = none; 1 = main plots (default);  2 = debugging plots, for setting up new study; 3 = all plots
 verbose.fig_output_file = 'PhysIO_output.ps';
+
 
 %% 7. Run the main script with defined parameters
 
@@ -83,6 +85,3 @@ physio.model        = model;
 physio.verbose      = verbose;
 
 [physio_out, R, ons_secs] = physio_main_create_regressors(physio);
-
-%[R, ons_secs] = physio_main_create_regressors(files, ...
-%    thresh, sqpar, order, verbose);
