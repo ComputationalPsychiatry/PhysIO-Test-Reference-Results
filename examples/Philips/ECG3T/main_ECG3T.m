125c475fa0c7bf0faeeec002d6910a71e1cf2f38
Lars Kasper <kasper@biomed.ee.ethz.ch>
2013-08-28 16:29:44 +0000

    PhysIO: bug with counting from end of SCANPHYSLOG.log (didn't do it, if vol_spacing was set), some error/warning output improvements plus documentation
    git-svn-id: svn+ssh://colombo07.ethz.ch/home/kasperla/svn_repos/smoothing/trunk/tSNR_fMRI_SPM/CheckPhysRETROICOR/PhysIOToolbox@251 d7055aa0-e31f-11dd-b94e-b50442429228

--- examples/Philips/ECG3T/main_ECG3T.m
+++ examples/Philips/ECG3T/main_ECG3T.m
@@ -89,8 +89,8 @@ model.output_multiple_regressors = 'multiple_regressors.txt';
 
 %% 6. Output Figures to be generated
 
-verbose.level           = 2; % 0 = none; 1 = main plots (default);  2 = debugging plots, for setting up new study; 3 = all plots
-verbose.fig_output_file = 'PhysIO_output_level2.tiff';
+verbose.level           = 1; % 0 = none; 1 = main plots (default);  2 = debugging plots, for setting up new study; 3 = all plots
+verbose.fig_output_file = 'PhysIO_output_level2.fig'; % Physio.tiff, .ps, .fig possible
 
 
 %% 7. Run the main script with defined parameters
