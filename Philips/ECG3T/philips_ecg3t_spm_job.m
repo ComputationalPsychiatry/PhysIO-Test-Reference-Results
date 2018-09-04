bf3afab9fa5c24ff1c4bcb0c52a93bf4714c7460
Lars Kasper <kasper@biomed.ee.ethz.ch>
2018-09-04 17:33:22 +0200

    updated spm job examples after running tests for .m-job files

--- Philips/ECG3T/philips_ecg3t_spm_job.m
+++ Philips/ECG3T/philips_ecg3t_spm_job.m
@@ -1,6 +1,6 @@
 %-----------------------------------------------------------------------
-% Job saved on 23-Jul-2015 17:49:08 by cfg_util (rev $Rev$)
-% spm SPM - SPM12 (6225)
+% Job saved on 04-Sep-2018 17:05:51 by cfg_util (rev $Rev: 6942 $)
+% spm SPM - SPM12 (7219)
 % cfg_basicio BasicIO - Unknown
 %-----------------------------------------------------------------------
 matlabbatch{1}.spm.tools.physio.save_dir = {''};
@@ -30,16 +30,17 @@ matlabbatch{1}.spm.tools.physio.preproc.cardiac.posthoc_cpulse_select.off = stru
 matlabbatch{1}.spm.tools.physio.model.output_multiple_regressors = 'multiple_regressors.txt';
 matlabbatch{1}.spm.tools.physio.model.output_physio = 'physio.mat';
 matlabbatch{1}.spm.tools.physio.model.orthogonalise = 'none';
+matlabbatch{1}.spm.tools.physio.model.censor_unreliable_recording_intervals = false;
 matlabbatch{1}.spm.tools.physio.model.retroicor.yes.order.c = 3;
 matlabbatch{1}.spm.tools.physio.model.retroicor.yes.order.r = 4;
 matlabbatch{1}.spm.tools.physio.model.retroicor.yes.order.cr = 1;
 matlabbatch{1}.spm.tools.physio.model.rvt.no = struct([]);
 matlabbatch{1}.spm.tools.physio.model.hrv.no = struct([]);
-matlabbatch{1}.spm.tools.physio.model.movement.yes.file_realignment_parameters ...
-    = {'rp_fMRI.txt'};
+matlabbatch{1}.spm.tools.physio.model.noise_rois.no = struct([]);
+matlabbatch{1}.spm.tools.physio.model.movement.yes.file_realignment_parameters = {'rp_fMRI.txt'};
 matlabbatch{1}.spm.tools.physio.model.movement.yes.order = 6;
-matlabbatch{1}.spm.tools.physio.model.movement.yes.outlier_translation_mm = 3;
-matlabbatch{1}.spm.tools.physio.model.movement.yes.outlier_rotation_deg = Inf;
+matlabbatch{1}.spm.tools.physio.model.movement.yes.censoring_method = 'MAXVAL';
+matlabbatch{1}.spm.tools.physio.model.movement.yes.censoring_threshold = [3 Inf];
 matlabbatch{1}.spm.tools.physio.model.other.no = struct([]);
 matlabbatch{1}.spm.tools.physio.verbose.level = 2;
 matlabbatch{1}.spm.tools.physio.verbose.fig_output_file = 'PhysIO_output.fig';
