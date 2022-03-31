75ad2163fe5f83f08e1f4a60f970c1f6ba5979d0
Lars Kasper <kasper@biomed.ee.ethz.ch>
2022-03-31 00:57:52 -0400

    Updated Examples and reference results for R2021a-v8.0.1, integration tests consistent with new reference results now but differ to v7.3 because of changes to respiratory filtering (bandpass) and ons_secs.fr

--- BIDS/CPULSE3T/bids_cpulse3t_matlab_script.m
+++ BIDS/CPULSE3T/bids_cpulse3t_matlab_script.m
@@ -64,7 +64,7 @@ physio.verbose.save_figs = false;
 physio.verbose.close_figs = false;
 physio.ons_secs.c_scaling = 1;
 physio.ons_secs.r_scaling = 1;
-physio.version = 'R2020a-v7.3.2';
+physio.version = 'R2021a-v8.0.1';
 
 %% Run physiological recording preprocessing and noise modeling
 physio = tapas_physio_main_create_regressors(physio);
